d8595cf2e2e402b491c87fe605e8d88ae6a670bd
Pho Hale <halechr@umich.edu>
2023-02-14 15:59:09 -0500

    added my scripts

---
+++ PhoCyKitRun-ForBrowser.bat
@@ -0,0 +1,5 @@
+
+CALL C:\Users\pho\Anaconda2\Scripts\activate.bat C:\Users\pho\Anaconda2\envs\CyKit-AnacondaPython36
+cd C:\Users\pho\bin\CyKit-master\Py3\
+Python.exe C:\Users\pho\bin\CyKit-master\Py3\CyKIT.py 127.0.0.1 5555 2
+PAUSE
---
+++ PhoCyKitRun.bat
@@ -0,0 +1,6 @@
+
+CALL C:\Users\pho\Anaconda2\Scripts\activate.bat C:\Users\pho\Anaconda2\envs\CyKit-AnacondaPython36
+cd C:\Users\pho\bin\CyKit-master\Py3\
+REM Python.exe C:\Users\pho\bin\CyKit-master\Py3\CyKIT.py 127.0.0.1 5555 2 openvibe+nocounter+noheader+nobattery+ovdelay:100+float+ovsamples:004
+Python.exe C:\Users\pho\bin\CyKit-master\Py3\CyKIT.py 127.0.0.1 5555 2 openvibe+generic+nocounter+noheader+nobattery+integer+ovsamples:004
+PAUSE
