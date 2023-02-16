e818c98c7a7167a86dfad269996b02d3c459e4be
Pho Hale <PhoHale@gmail.com>
2023-02-16 04:37:06 -0500

    updated scripts

--- PhoCyKitRun-ForBrowser.bat
+++ PhoCyKitRun-ForBrowser.bat
@@ -1,6 +1,15 @@
+@REM @REM Alienbook Pre 2023-02-16
+@REM CALL C:\Users\pho\Anaconda2\Scripts\activate.bat C:\Users\pho\Anaconda2\envs\CyKit-AnacondaPython36
+@REM cd C:\Users\pho\bin\CyKit-master\Py3\
+@REM Python.exe C:\Users\pho\bin\CyKit-master\Py3\CyKIT.py 127.0.0.1 5555 2
 
-CALL C:\Users\pho\Anaconda2\Scripts\activate.bat C:\Users\pho\Anaconda2\envs\CyKit-AnacondaPython36
-cd C:\Users\pho\bin\CyKit-master\Py3\
-Python.exe C:\Users\pho\bin\CyKit-master\Py3\CyKIT.py 127.0.0.1 5555 2
-@REM python CyKIT.py 127.0.0.1 5555 2
+@REM Apogee 2023-02-16
+cd C:\Users\pho\repos\EmotivEpoc\CyKit\Py3\
+C:\Users\pho\.pyenv\pyenv-win\versions\3.7.9\python.exe C:\Users\pho\repos\EmotivEpoc\CyKit\Py3\CyKIT.py 127.0.0.1 5555 2 info+outputdata
+@REM 2 stands for consumer model
+@REM pywinusb
+
+@REM 127.0.0.1:54123
+@REM python.exe .\CyKIT.py 127.0.0.1 54123 6 outputdata+noweb
+
 PAUSE
--- PhoCyKitRun.bat
+++ PhoCyKitRun.bat
@@ -1,6 +1,22 @@
+@REM @REM Alienbook Pre 2023-02-16
+@REM CALL C:\Users\pho\Anaconda2\Scripts\activate.bat C:\Users\pho\Anaconda2\envs\CyKit-AnacondaPython36
+@REM cd C:\Users\pho\bin\CyKit-master\Py3\
+@REM REM Python.exe C:\Users\pho\bin\CyKit-master\Py3\CyKIT.py 127.0.0.1 5555 2 openvibe+nocounter+noheader+nobattery+ovdelay:100+float+ovsamples:004
+@REM Python.exe C:\Users\pho\bin\CyKit-master\Py3\CyKIT.py 127.0.0.1 5555 2 openvibe+generic+nocounter+noheader+nobattery+integer+ovsamples:004
 
-CALL C:\Users\pho\Anaconda2\Scripts\activate.bat C:\Users\pho\Anaconda2\envs\CyKit-AnacondaPython36
-cd C:\Users\pho\bin\CyKit-master\Py3\
-REM Python.exe C:\Users\pho\bin\CyKit-master\Py3\CyKIT.py 127.0.0.1 5555 2 openvibe+nocounter+noheader+nobattery+ovdelay:100+float+ovsamples:004
-Python.exe C:\Users\pho\bin\CyKit-master\Py3\CyKIT.py 127.0.0.1 5555 2 openvibe+generic+nocounter+noheader+nobattery+integer+ovsamples:004
+
+@REM Apogee 2023-02-16
+@REM pyenv install 3.7.9
+
+cd C:\Users\pho\repos\EmotivEpoc\CyKit\Py3\
+@REM pyenv local 3.7.9 @REM # Breaks Execution
+@REM pyenv exec python --version
+@REM pyenv exec python 
+
+@REM # Find Python Executable Location by doing:
+@REM C:\Users\pho\repos\EmotivEpoc\CyKit\Py3>pyenv which python
+@REM 	C:\Users\pho\.pyenv\pyenv-win\versions\3.7.9\python.exe
+
+C:\Users\pho\.pyenv\pyenv-win\versions\3.7.9\python.exe C:\Users\pho\repos\EmotivEpoc\CyKit\Py3\CyKIT.py 127.0.0.1 5555 2 openvibe+generic+nocounter+noheader+nobattery+integer+ovsamples:004
+
 PAUSE
