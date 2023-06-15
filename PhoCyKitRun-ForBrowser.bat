5f52c19c0f1388c0ec674ac9521400e12dc50211
Pho Hale <PhoHale@gmail.com>
2023-06-15 19:05:13 -0400

    bump

--- PhoCyKitRun-ForBrowser.bat
+++ PhoCyKitRun-ForBrowser.bat
@@ -6,6 +6,7 @@
 @REM Apogee 2023-02-16
 cd C:\Users\pho\repos\EmotivEpoc\CyKit\Py3\
 C:\Users\pho\.pyenv\pyenv-win\versions\3.7.9\python.exe C:\Users\pho\repos\EmotivEpoc\CyKit\Py3\CyKIT.py 127.0.0.1 5555 2
+@REM I'm confused because I thought it was supposed to be 6 for the Epoc+
 @REM info+generic+outputdata
 @REM 2 stands for consumer model
 @REM pywinusb
