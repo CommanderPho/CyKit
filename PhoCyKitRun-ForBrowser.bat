1042850826919b75a54b5c654c06c04a35c07489
Pho Hale <PhoHale@gmail.com>
2023-06-21 17:50:53 -0400

    MINOR

--- PhoCyKitRun-ForBrowser.bat
+++ PhoCyKitRun-ForBrowser.bat
@@ -7,7 +7,8 @@
 cd C:\Users\pho\repos\EmotivEpoc\CyKit\Py3\
 @REM C:\Users\pho\.pyenv\pyenv-win\versions\3.7.9\python.exe C:\Users\pho\repos\EmotivEpoc\CyKit\Py3\CyKIT.py 127.0.0.1 5555 2
 @REM I'm confused because I thought it was supposed to be 6 for the Epoc+
-C:\Users\pho\.pyenv\pyenv-win\versions\3.7.9\python.exe C:\Users\pho\repos\EmotivEpoc\CyKit\Py3\CyKIT.py 127.0.0.1 5555 2 +verbose
+@REM C:\Users\pho\.pyenv\pyenv-win\versions\3.7.9\python.exe C:\Users\pho\repos\EmotivEpoc\CyKit\Py3\CyKIT.py 127.0.0.1 5555 2 info+pywinusb+verbose
+C:\Users\pho\.pyenv\pyenv-win\versions\3.7.9\python.exe C:\Users\pho\repos\EmotivEpoc\CyKit\Py3\CyKIT.py 127.0.0.1 5555 5 info+verbose
 
 @REM Use the correct Keymodel (6 is used for EPOC+ in 16-bit mode, 7 would be used for EPOC+ 14-bit EPOC mode)
 @REM Premium models are likely prototypes, that are not often used. If you are in doubt, always try the consumer model.
