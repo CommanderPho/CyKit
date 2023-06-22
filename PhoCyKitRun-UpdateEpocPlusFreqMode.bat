e1f753934de420b044be6c18ed4f853607365ef6
Pho Hale <PhoHale@gmail.com>
2023-06-21 22:09:38 -0400

    added version of the run script for setting the Epoc+ frequency mode when plugged in over USB

---
+++ PhoCyKitRun-UpdateEpocPlusFreqMode.bat
@@ -0,0 +1,5 @@
+@REM Apogee 2023-06-21
+cd C:\Users\pho\repos\EmotivEpoc\CyKit\Py3\
+C:\Users\pho\.pyenv\pyenv-win\versions\3.7.9\python.exe C:\Users\pho\repos\EmotivEpoc\CyKit\Py3\CyKIT.py 127.0.0.1 5555 6 +pywinusb 
+
+PAUSE
